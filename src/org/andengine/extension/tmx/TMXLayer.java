3070100521c1e9d8fa528a42523692e725020a7b
n2a5me <b1517.cbq@gmail.com>
2013-06-23 23:33:10 +0700

    commit

--- src/org/andengine/extension/tmx/TMXLayer.java
+++ src/org/andengine/extension/tmx/TMXLayer.java
@@ -270,22 +270,26 @@ public class TMXLayer extends SpriteBatch implements TMXConstants {
 		final int tileHeight = this.mTMXTiledMap.getTileHeight();
 		final int tileWidth = this.mTMXTiledMap.getTileWidth();
 
-		if(this.mTexture == null) {
-			this.mTexture = tmxTileTextureRegion.getTexture();
-			super.initBlendFunction(this.mTexture);
-		} else {
-			if(this.mTexture != tmxTileTextureRegion.getTexture()) {
-				throw new AndEngineRuntimeException("All TMXTiles in a TMXLayer need to be in the same TMXTileSet.");
+		if (tmxTileTextureRegion != null) {
+			// Unless this is a transparent tile, setup the texture
+			if (this.mTexture == null) {
+				this.mTexture = tmxTileTextureRegion.getTexture();
+				super.initBlendFunction(this.mTexture);
+			} else {
+				if (this.mTexture != tmxTileTextureRegion.getTexture()) {
+					throw new AndEngineRuntimeException("All TMXTiles in a TMXLayer need to be in the same TMXTileSet.");
+				}
 			}
 		}
 		final TMXTile tmxTile = new TMXTile(pGlobalTileID, column, row, tileWidth, tileHeight, tmxTileTextureRegion);
 		tmxTiles[row][column] = tmxTile;
 
+		
+
+		if(pGlobalTileID != 0) {
 		this.setIndex(this.getSpriteBatchIndex(column, row));
 		this.drawWithoutChecks(tmxTileTextureRegion, tmxTile.getTileX(), tmxTile.getTileY(), tileWidth, tileHeight, Color.WHITE_ABGR_PACKED_FLOAT);
 		this.submit(); // TODO Doesn't need to be called here, but should rather be called in a "init" step, when parsing the XML is complete.
-
-		if(pGlobalTileID != 0) {
 			/* Notify the ITMXTilePropertiesListener if it exists. */
 			if(pTMXTilePropertyListener != null) {
 				final TMXProperties<TMXTileProperty> tmxTileProperties = tmxTiledMap.getTMXTileProperties(pGlobalTileID);
